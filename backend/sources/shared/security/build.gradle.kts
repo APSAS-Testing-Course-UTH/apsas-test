e53b635601f7cf4abdb0cf451b63f8807e980098
Nguyễn Thanh Tân <me@tozydev.id.vn>
2026-03-28 21:28:38 +0700

    test(evaluation-shared): add unit tests for service and utils classes (#10)

--- backend/sources/shared/security/build.gradle.kts
+++ backend/sources/shared/security/build.gradle.kts
@@ -7,4 +7,8 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter-webflux")
 
     api("org.springframework.boot:spring-boot-starter-security")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 }
