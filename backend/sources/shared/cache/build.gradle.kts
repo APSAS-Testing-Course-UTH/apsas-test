c7675216e628628097f0f40dc948ffb57a0df351
NguyenThanhChien-2010 <thanhhchienn2636@gmail.com>
2026-04-05 00:04:43 +0700

    fix(cache): add coverage tests for cache config

--- backend/sources/shared/cache/build.gradle.kts
+++ backend/sources/shared/cache/build.gradle.kts
@@ -6,4 +6,6 @@ dependencies {
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     api("org.springframework.boot:spring-boot-starter-cache")
     api("org.springframework.boot:spring-boot-starter-data-redis")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
